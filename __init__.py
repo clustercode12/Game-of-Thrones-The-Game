d69b8e9e6ce42cae7db879f7be3834a85ca9ab4f
ale9gg <75221905+ale9gg@users.noreply.github.com>
2020-12-14 15:57:46 +0100

    a

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
 from army import Army
 from general import General
 from turn import Turn
+<<<<<<< Updated upstream
 import armyDicctionaries as Dict
 
 westerosArmy = Army(Dict.WESTEROS_ARMY)
@@ -9,3 +10,15 @@ print(westerosArmy)
 turn = Turn(targaryenArmy, westerosArmy, True, 100, Dict.WEAKEST_FIRST, Dict.FULL_ATTACK, Dict.SUNSPEAR)
 #print(turn.attackerArmy)
 #print(turn.defensorArmy.getArmyFromLocation(Dict.THE_WALL))
+=======
+
+westerosArmy = Army(Army.WESTEROS_ARMY)
+targaryenArmy = Army(Army.TARGARYEN_ARMY)
+
+
+print(targaryenArmy)
+print(westerosArmy)
+
+turn = Turn(targaryenArmy, westerosArmy, True, 10, 1, 1, 1, )
+print(turn.attackerArmy)
+>>>>>>> Stashed changes
