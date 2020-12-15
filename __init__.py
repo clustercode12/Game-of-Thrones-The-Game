2c6a8546a577192ae61ad0ec60eb74e7c9755240
clustercode <clustercode12@gmail.com>
2020-12-15 18:41:53 +0100

    Bug fixes

--- __init__.py
+++ __init__.py
@@ -6,9 +6,11 @@ import armyDicctionaries as Dict
 westerosArmy = Army(Dict.WESTEROS_ARMY)
 targaryenArmy = Army(Dict.TARGARYEN_ARMY)
 
+turns = []
 for i in range(100):
     turn = Turn(targaryenArmy, westerosArmy, True, 3, Dict.STRONGEST_FIRST, Dict.FULL_ATTACK, Dict.SUNSPEAR)
+    turns.append(turn)
+    
+print(turns[3].defensorArmy)
+print(turns[90].defensorArmy)
 
-
-print(targaryenArmy)
-print(westerosArmy)
